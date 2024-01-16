dfff91924a5de7810bc8977ab79d03c1ebb50800
Kanisorn-S <karamahati@gmail.com>
2024-01-16 14:59:54 +0700

    pygame window

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import pygame as pg
+
+pg.init()
+
+SIZE = (640, 640)
+
+window = pg.display.set_mode(SIZE)
+
+run = True
+
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+    window.fill('orange')
+    pg.display.update()
+
+print("End of program")
+pg.quit()
